ff942871e7311d00512dbd87a5be0257005673e5
Zaki <mamadovzicx@gmail.com>
2024-08-28 14:37:11 +0300

    Add utility function
    - Mainly used to convert camel-cased-exceptions into snake case, to call its corresponding handler automatically.

--- rest_framework_toolbox/core/utils.py
+++ rest_framework_toolbox/core/utils.py
@@ -11,3 +11,11 @@ def get_class_name(obj):
 
 def get_class_fields(_class):
     return [attr for attr in dir(_class) if not callable(getattr(_class, attr)) and not attr.startswith("__") and not attr.startswith("_")]
+
+
+def camel_to_snake(name):
+    import re
+    # Find all uppercase letters and add an underscore before them
+    s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
+    # Convert the entire string to lowercase
+    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
