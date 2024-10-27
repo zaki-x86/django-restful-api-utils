56b9e83f51acd12893f53a9929903e9ab6c0ca13
Zaki <mamadovzicx@gmail.com>
2024-10-27 17:20:51 +0300

    Update error_handler
    - Add logging for errors
    - Add logging options for crash and status code > 499

--- rest_framework_toolbox/handlers/error_handler/error_handler.py
+++ rest_framework_toolbox/handlers/error_handler/error_handler.py
@@ -3,10 +3,13 @@ from django.conf import settings
 from rest_framework_toolbox.core.utils import import_class
 from rest_framework_toolbox.core.models import JSONModel
 from .main import ErrorHandler
+from rest_framework.response import Response
 
 __all__ = [
     'exception_handler',
 ]
+
+handler = ErrorHandler()
 
 def get_error_model(view = None):
     if view and hasattr(view, 'error_model'):
@@ -17,15 +20,15 @@ def get_error_model(view = None):
         raise Exception("A global error json model must be set")
     
 def exception_handler(exc, context):
-    # WARNING: Don't remove the default handler as it performs DB rollback
-    response = drf_exception_handler(exc, context)
-    assert response, "DRF Exception handler didn't return a response, this could be due to the exception thrown is not an instance of excpetions.APIException"
     try:
+        # WARNING: Don't remove the default handler as it performs DB rollback
+        response = drf_exception_handler(exc, context)
+        assert response, "DRF Exception handler didn't return a response, this could be due to the exception thrown is not an instance of excpetions.APIException"
         error_model = get_error_model(view = context['view'])
         assert error_model, "Error JSON model not found"
         assert issubclass(error_model, JSONModel), f"error class: {error_model.__class__.__name__} must be an extension to JSONModel"
         
-        handler = ErrorHandler(error_model=error_model)
+        handler.error_model = error_model
         error_res = handler._handle(exc, context, response)
         assert error_res, "Error handler did not return a response"
         assert isinstance(error_res, error_model), f"handler response must be an instance of {settings.ERROR_JSON_MODEL}"
@@ -35,18 +38,30 @@ def exception_handler(exc, context):
             response.headers.update(exc.headers)
         if getattr(exc, 'callback', None):
             response.add_post_render_callback(exc.callback)
+        
         return response
         
     except Exception as e:
-        #TODO capture traceback and log this error, then send it to sentry/dashboard
-        import traceback
-        traceback.print_exc()
+        handler.context = context
+        handler.exc = exc
         if settings.DEBUG:
-            raise e
-        else:
-            response.status_code = 500
-            response.data = {
-                'code': 'Error in handling error',
-                'details': 'An unexpected error occurred during handling an error.'
+            import traceback
+            status_code = 500
+            data = {
+                'code': 'system_error',
+                'details': str(e),
+                'traceback': traceback.format_exc()
             }
-            return response
+            handler.report_crash()
+        else:
+            handler.report_crash()
+            status_code = 500
+            data = {
+                'code': 'system_error',
+                'details': 'An unexpected error occurred, and it is being investigated now.'
+            }
+        
+        return Response(
+            status=status_code,
+            data=data
+        )
--- rest_framework_toolbox/handlers/error_handler/main.py
+++ rest_framework_toolbox/handlers/error_handler/main.py
@@ -1,6 +1,10 @@
+from rest_framework_toolbox.core.models import JSONModel
+from rest_framework_toolbox.core.utils import import_class, get_class_fields, camel_to_snake
 from typing import Any, Dict, TypeVar, Callable
 
 from ._config import configs
+
+import logging
 
 __testing = configs['test']
 
@@ -9,9 +13,6 @@ if not __testing:
     from rest_framework.response import Response
     from rest_framework.views import exception_handler as drf_exception_handler
     from django.conf import settings
-    
-from rest_framework_toolbox.core.utils import import_class, get_class_fields, camel_to_snake
-from rest_framework_toolbox.core.models import JSONModel
 
 
 __all__ = [
@@ -21,7 +22,6 @@ __all__ = [
 if __testing:
     APIException = TypeVar("APIException")
     Response = TypeVar("Response")
-
 
 
 # def register_handler(exception_class):
@@ -42,18 +42,25 @@ class ErrorHandler:
         'MethodNotAllowed',
         'PermissionDenied',
         'ServiceUnavailable',
-        'Http404'
+        'Http404',
     ]
-    
+
     _user_handlers = {}
-    
+
     class Config:
         use_drf_handler = False
-        
-    def __init__(self, error_model = None) -> None:
+
+    def __init__(self, error_model=None) -> None:
         if error_model:
-            assert issubclass(error_model, JSONModel), "error_model must be an extension to JSONModel"
+            assert issubclass(
+                error_model, JSONModel), "error_model must be an extension to JSONModel"
         self.error_model = error_model
+        self.context = None
+        self.exc = None
+        self.response = None
+        self.logger = ErrorHandler.register_logger()
+        self.crash_logger = ErrorHandler.register_crash_logger()
+        self.signals = ErrorHandler.register_signals()
 
     @classmethod
     def register_handler(cls, exception_class):
@@ -63,10 +70,10 @@ class ErrorHandler:
         return decorator
 
     @classmethod
-    def register_exception(cls, exception : APIException):
+    def register_exception(cls, exception: APIException):
         @cls.register_handler(exception)
         def handler(exc, context, res):
-            res.message  = exc.outcome
+            res.message = exc.outcome
             res.data = {
                 'code': exc.get_full_details()['code'],
                 'details': exc.get_full_details()['message']
@@ -75,12 +82,12 @@ class ErrorHandler:
             return res
 
     @classmethod
-    def register_exceptions(cls, exceptions : list):
+    def register_exceptions(cls, exceptions: list):
         for exception in exceptions:
             cls.register_exception(exception)
 
     @staticmethod
-    def get_error_model(view = None):
+    def get_error_model(view=None):
         if view and hasattr(view, 'error_model'):
             return view.error_model
         elif getattr(settings, 'ERROR_JSON_MODEL', None):
@@ -88,9 +95,32 @@ class ErrorHandler:
         else:
             raise Exception("A global error json model must be set")
 
+    @classmethod
+    def register_signals(self):
+        signals = getattr(settings, 'ERROR_HANDLER_SIGNALS', None)
+
+        if type(signals) == list:
+            return signals
+
+    @classmethod
+    def register_logger(self):
+        logger = getattr(settings, 'ERROR_HANDLER_LOGGER', None)
+
+        if logger:
+            return import_class(logger)
+
+    @classmethod
+    def register_crash_logger(self):
+        crash_logger = getattr(settings, 'ERROR_HANDLER_CRASH_LOGGER', None)
+
+        if crash_logger:
+            return import_class(crash_logger)
+        else:
+            return logging.getLogger('rest_framework_toolbox_crash_logger')
+
     def _convert_to_apiexception(self, exc):
         return APIException(
-            
+
         )
 
     def _get_error_model_fields(self) -> dict:
@@ -98,7 +128,7 @@ class ErrorHandler:
         fields = []
         for attr in dir(self.error_model):
             if callable(getattr(self.error_model, attr)) and issubclass(getattr(self.error_model, attr).__class__, JSONModel):
-              fields.append(attr)  
+                fields.append(attr)
             elif not callable(getattr(self.error_model, attr)) and not attr.startswith("__") and not attr.startswith("_"):
                 fields.append(attr)
             else:
@@ -111,9 +141,10 @@ class ErrorHandler:
     def _handle_with_registered_handler(self, exception_name: str, exc, context, response):
         if exception_name in self._user_handlers:
             user_handler = self._user_handlers[exception_name]
-            # User handler must return an instance of self.error_model class
+            # User handler must return an instance of `self.error_model` class
             error_res = user_handler(exc, context, response)
-            assert isinstance(error_res, self.error_model), "User-defined handler must return an instance of the error model"
+            assert isinstance(
+                error_res, self.error_model), "User-defined handler must return an instance of the error model"
 
             return error_res
 
@@ -126,7 +157,8 @@ class ErrorHandler:
 
             # Handle default exception using handler defined in error_res
             if getattr(error_res, camel_to_snake(exception_class), None):
-                handler = getattr(error_res, camel_to_snake(exception_class), None)
+                handler = getattr(
+                    error_res, camel_to_snake(exception_class), None)
                 if callable(handler):
                     error_res = handler(context['request'], response)
                     if error_res:
@@ -136,67 +168,171 @@ class ErrorHandler:
 
             return None
 
-    def _handle(self, exc : APIException, context: Dict, response: Response) -> JSONModel:
+    def _handle(self, exc: APIException, context: Dict, response: Response) -> JSONModel:
+        """
+        Django REST API Error handler
+
+        In the upcoming sections, between parantheses defined the phase name, in which a signal can be registered
+
+        # Handling API exceptions goes in these phases:
+        ----------------------------------------
+        ## Initialization (init)
+        -------------------------
+        - Get the view that threw the error
+        - Get the exception class name
+        - Instantiate the error json model
+
+        ## Error Handling (error_handling)
+        ----------------------------------
+        ### Handling with registered handlers
+        -------------------------------------
+        - Check if the user registered any custom handler against the exception
+        - If a handler is found, call it and return the result
+
+        ### Handle non APIException errors
+        ----------------------------------
+        - Check if the exception is an instance of Http404 which is a standard exception but not an APIException
+        - Check if the custom exception has its own handler and return it
+
+        ### Handle exception with view's `on_error` func
+        --------------------------------------------
+        - Check if the view has an `on_error` method
+        - If `on_error` is defined, call it and return the result
+
+        ### Fallback to default handlers
+        ---------------------------------
+        - If no handler is defined, or `on_error` failed to handle.
+        - Handle the error using the exception class user supplied attributes
+
+        ## Failure to handle error (failure)
+        ------------------------------------
+        - If no handler is defined, raise an exception
+        ---------------------------
+
+        """
+        self.context = context
+        self.exc = exc
+        if response:
+            self.response = response
         view = context.get('view', None)
         exception_class = exc.__class__.__name__
         error_res = None
 
         assert view, "View is required to handle exceptions"
         assert self.error_model, "Error model is required to handle exceptions"
-
-        #print(f"Handling exception {exception_class}")
         
-        # 1. Check if the user registered any custom handler against the exception 
-        error_res = self._handle_with_registered_handler(exception_class, exc, context, response)
-        if error_res:
+        if response.status_code > 499:
+            self.report_crash()
 
+        # 1. Check if the user registered any custom handler against the exception
+        error_res = self._handle_with_registered_handler(
+            exception_class, exc, context, response)
+        if error_res:
+            self._post_handling_processing()
             return error_res
 
         # If the error is an instance of Http404 which is a standard exception but not an APIException
         if exception_class == 'Http404':
-            print("Handling with predefined handler")
-            error_res = self._handle_with_predefined_handler(exception_class, exc, context, response)
+            error_res = self._handle_with_predefined_handler(
+                exception_class, exc, context, response)
 
             if error_res:
+                self._post_handling_processing()
+                return error_res
+
+        elif exception_class == 'ServiceUnavailable':
+            error_res = self._handle_with_predefined_handler(
+                exception_class, exc, context, response)
+
+            if error_res:
+                self._post_handling_processing()
                 return error_res
 
         # 2. Check if the exception can be handled by view's 'on_error' method
-        if getattr(view, 'on_error', None):
-            print(f"Handling exception {exception_class} with on_error")
+        if getattr(view, 'on_error', None) and isinstance(exc, APIException):
             serializer_error_handler = getattr(view, 'on_error', None)
             if serializer_error_handler and callable(serializer_error_handler):
                 error_res = serializer_error_handler(exc, context, response)
-                
+
             if error_res:
                 assert isinstance(error_res, self.error_model), \
                     "User-defined handler must return an instance of the error model"
-                
+                self._post_handling_processing()
                 return error_res
-        
+
         # 3. Fallback to default handlers defined in error_res if no `on_error` is defined
-        error_res = self._handle_with_predefined_handler(exception_class, exc, context, response)
+        error_res = self._handle_with_predefined_handler(
+            exception_class, exc, context, response)
 
         if error_res:
+            self._post_handling_processing()
             return error_res
-        
-        # 3. If no handler is defined, handle the error using the exception class user supplied attributes    
-        else:
-            error_response_fields = self._get_error_model_fields()
-            error_res = self.error_model()
 
-            for field in error_response_fields:
-                # Check if the field value is set directly in the exc class
-                if getattr(exc, field, None):
-                    val = getattr(exc, field, None)
-                    if val and callable(val):
-                        val = val(context, exc.get_full_details())
-                    setattr(error_res, field, val)
+        # 3. If no handler is defined, handle the error using the exception class user supplied attributes
+        else:
+            self.report_crash()
+            error_res = self.error_model()
 
         # 4. Postprocessing
         if error_res:
+            self._post_handling_processing()
             return error_res
         else:
-            raise Exception(f"Error could not be handled:\n{exc.get_full_details()}")
+            raise Exception(f"Error could not be handled:\n{str(exc)}")
+
+    def _post_handling_processing(self):
+        view = None
+        request = None
+        if self.context:
+            view = self.context.get('view', None)
+            request = self.context.get('request', None)
+        if view and request:
+            try:
+                req_data = request.data
+            except:
+                req_data = None
+                req_method = None
+                uri = None
+                meta = None
+            req_method = getattr(request, 'method', None)
+            uri = getattr(request, 'path_info', None)
+            meta = getattr(request, 'META', None)
+            
+            import traceback
+
+            self.logger.warn(
+                f'Exception raised in {view.__class__.__name__} during handling {req_method} {uri} {req_data} {meta} - Taceback: {traceback.format_exc()}')
+
+    def report_crash(self):
+        import traceback
+        traceback_str = traceback.format_exc()
+        request = self.context.get('request', None)
+        method = None
+
+        if request:
+            method = request.method
+            req_data = getattr(request, 'data', None)
+            req_meta = getattr(request, 'META', None)
+            request = request.build_absolute_uri()
+
+        view = self.context.get('view', None)
+        
+        if view:
+            view = view.__class__.__name__
+            
+        exception_name = self.exc.__class__.__name__
+        
+        self.crash_logger.error(
+                            "--- Error crash report --- "
+                            f"\nError context:\n" 
+                            "------------------\n"
+                            f"Request Header:\n{method} {request}\n\n"
+                            f"Request Data:\n{req_data}\n\n"
+                            f"Request META:\n{req_meta}\n\n"
+                            f"Action Name:\n{view}\n\n"
+                            f"Exception Class:\n{exception_name}\n\n"
+                            f"Traceback Record:\n{traceback_str}\n\n"
+                            )
 
     @staticmethod
     def handle_exception(exc, context) -> Response:
@@ -204,10 +340,11 @@ class ErrorHandler:
         # WARNING: Don't remove the default handler as it performs DB rollback and init headers
         self = ErrorHandler()
         response = drf_exception_handler(exc, context)
-        self.error_model = self.get_error_model(view = context['view'])
+        self.error_model = self.get_error_model(view=context['view'])
         assert self.error_model, "Error model must be set"
-        assert issubclass(self.error_model, JSONModel), "error_model must be an extension to JSONModel"
-        
+        assert issubclass(
+            self.error_model, JSONModel), "error_model must be an extension to JSONModel"
+
         try:
             error_res = self._handle(exc, context, response)
             if error_res:
@@ -216,7 +353,7 @@ class ErrorHandler:
                     response.headers.update(exc.headers)
                 return response
         except:
-            #TODO capture traceback and log this error, then send it to sentry/PROMETHEOUS
+            # TODO capture traceback and log this error, then send it to sentry/PROMETHEOUS
             print(response)
             import traceback
             traceback.print_exc()
